3954220206c5a0c241fa5da048b6ff4f9d6d8a3f
Aromer62823 <62823aromero4@gmail.com>
2026-09-11 17:21:19 -0600

    Removed SECRET_KEY, and turned off debug mode

--- project/config/settings.py
+++ project/config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
+# Secret Key from .env
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*f^13tw(m@d%h+jrx8mty44i0z*d+bn3wa5=e*#%^#z+mwjlo_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
