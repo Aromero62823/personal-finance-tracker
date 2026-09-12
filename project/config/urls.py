e6b08fd7527357c81746ea08122b4638f7e14786
Angel Romero <89607800+Aromero62823@users.noreply.github.com>
2026-09-11 18:38:17 -0600

    Add re_path import to urls.py

--- project/config/urls.py
+++ project/config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('', include("useraccounts.urls")), # Login/Registration made for user login and registration
